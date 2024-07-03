d8a7dcefe36387e0b1c8a475c358ca4fb6b59b7c
FarhanAliQureshi <52462581+FarhanAliQureshi@users.noreply.github.com>
2024-07-04 01:37:21 +0500

    Windows_sys and Windows-subsystem added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+#![windows_subsystem = "windows"]
+
+use windows_sys::{
+    core::*,
+    Win32::UI::WindowsAndMessaging::*,
+};
+
 fn main() {
-    println!("Hello, world!");
+    unsafe {
+        MessageBoxA(0, s!("Ansi"), s!("Caption"), MB_OK);
+        MessageBoxW(0, w!("Wide"), w!("Caption"), MB_OK);
+    }
 }
