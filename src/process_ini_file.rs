89946feba0a8e7890e369b5c0a34d6526154ed24
FarhanAliQureshi <52462581+FarhanAliQureshi@users.noreply.github.com>
2024-07-05 02:07:10 +0500

    Find and reset key value

--- src/process_ini_file.rs
+++ src/process_ini_file.rs
@@ -22,3 +22,67 @@ pub fn read_lines_from_file(filename: &String) -> Option<Vec<String>> {
     
     Some(lines)
 }
+
+pub fn reset_keys_values(lines: &Vec<String>, keys: &Vec<String>) -> Vec<String> {
+    let mut modified_lines: Vec<String> = Vec::new();
+
+    for line in lines {
+        // Trim all whie spaces from line
+        let trimmed = line.trim();
+        
+        // Ignore empty lines
+        if trimmed == "" {
+            modified_lines.push(line.clone());
+            continue;
+        }
+        // Ignore comments
+        if trimmed.chars().nth(0).unwrap() == '#' {
+            modified_lines.push(line.clone());
+            continue;
+        }
+        if trimmed.chars().nth(0).unwrap() == ';' {
+            modified_lines.push(line.clone());
+            continue;
+        }
+        // Ignore section declaration
+        if trimmed.chars().nth(0).unwrap() == '[' {
+            modified_lines.push(line.clone());
+            continue;
+        }
+        // Confirm it's a key value line
+        if trimmed.contains("=") {
+            // Does the key matches with one of given keys' names
+            if key_matches(trimmed, &keys) {
+                // If key name matches then reset it's value
+                modified_lines.push(reset_key(&line));
+            } else {
+                // If key name doesn't match then ignore the line
+                modified_lines.push(line.clone());
+            }
+        } else {
+            // If line is not a key value then ignore it
+            modified_lines.push(line.clone());
+        }
+    }
+
+    modified_lines
+}
+
+fn key_matches(line: &str, keys: &Vec<String>) -> bool {
+    let (line_key, _) = line.split_once("=").unwrap();
+
+    for key in keys {
+        // Ignore text case
+        if line_key.to_lowercase() == key.to_lowercase() {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+fn reset_key(line: &String) -> String {
+    let (key, _) = line.split_once("=").unwrap();
+
+    key.to_string() + "="
+}
