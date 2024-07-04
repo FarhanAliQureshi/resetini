89b0800a75ec90f4d4dc5bd09a78335b4ca40442
FarhanAliQureshi <52462581+FarhanAliQureshi@users.noreply.github.com>
2024-07-04 17:03:07 +0500

    Parsing command-line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,60 @@
 #![windows_subsystem = "windows"]
 
-use windows_sys::{
-    core::*,
-    Win32::UI::WindowsAndMessaging::*,
-};
+mod msgboxfunc;
+
+use msgboxfunc::*;
 
 fn main() {
-    unsafe {
-        MessageBoxA(0, s!("Ansi"), s!("Caption"), MB_OK);
-        MessageBoxW(0, w!("Wide"), w!("Caption"), MB_OK);
+    let args: Vec<String> = std::env::args().collect();
+
+    // display_string_vector(&args);
+    // return;
+
+    // If no command-line arguments are given then display help and quit
+    if args.len() == 1 {
+        display_help();
+        return;
     }
+    // If user asked to display help then display help and quit
+    if args.len() > 1 {
+        if args[1] == "/?" || args[1].to_lowercase() == "/h" || args[1].to_lowercase() == "-h" {
+            display_help();
+            return;
+        }
+    }
+    // INI Filename is in args[1]. After that all remaining arguments are key names
+    // If user didn't give key names after the filename then give error and exit
+    if args.len() == 2 {
+        err_msgbox(&String::from("No key names are given."), None);
+        return;
+    }
+
+    // With above filters, at this point, we are sure that the args length is greater than 1,
+    // therefore, we are sure we can access 2nd element.
+    let filename = args[1].clone();
+    msgbox(&filename, Some(&"Filename".to_string()));
+
+    // We are also sure that args length is greater than 2. Everything after filename
+    // are key names.
+    let key_names: Vec<String> = args[2..].to_vec();
+    display_string_vector(&key_names);
+
+}
+
+fn display_help() {
+    let mut help = String::from("Usage:\n\nResetINI <filename> <key_name1> <key_name2> ...");
+    help += "\n\n\n";
+    help += "<filename>\tINI Filename with path\n";
+    help += "<key_name1>\tKey name to reset it's value\n";
+    help += "\n\nYou can give as many key names as needed after the filename, separated by a space.";
+
+    info_msgbox(&help, None);
+}
+
+fn display_string_vector(strvec: &Vec<String>) {
+    // For testing. Merge all elements of given string vector and display 
+    // it in a message box.
+    let mut message = strvec.join("\n");
+    message.push_str("\0"); 
+    msgbox(&message, None);
 }
---
+++ src/msgboxfunc.rs
@@ -0,0 +1,39 @@
+use windows_sys::Win32::UI::WindowsAndMessaging::*;
+
+const MSGBOX_CAPTION: &str = "Reset INI";
+
+fn win_msgbox(message: &String, caption: Option<&String>, style: Option<&u32>) {
+    // Prepare a default caption string from constant
+    let default_caption = String::from(MSGBOX_CAPTION);
+    // If no optional caption is given (None) then use default caption string
+    let expanded_caption = caption.unwrap_or(&default_caption);
+
+    // Win32API expect c-style string which are terminated with null character
+    // Therefore, add null character to message and caption strings.
+    let c_msg = format!("{}\0", message);
+    let c_caption = format!("{}\0", expanded_caption);
+
+    // If not msgbox style is given then use default style of just an OK button
+    let expanded_style = style.unwrap_or(&MB_OK).clone();
+
+    // Calling Win32 API is unsafe 
+    unsafe {
+        // Ignore the return value using underscore
+        _ = MessageBoxA(0, c_msg.as_ptr(), c_caption.as_ptr(), 
+            expanded_style);
+    }
+}
+
+pub fn msgbox(message: &String, caption: Option<&String>) {
+    win_msgbox(message, caption, Some(&MB_OK));
+}
+
+pub fn err_msgbox(message: &String, caption: Option<&String>) {
+    let style = MB_OK | MB_ICONERROR;
+    win_msgbox(message, caption, Some(&style));
+}
+
+pub fn info_msgbox(message: &String, caption: Option<&String>) {
+    let style = MB_OK | MB_ICONINFORMATION;
+    win_msgbox(message, caption, Some(&style));
+}
